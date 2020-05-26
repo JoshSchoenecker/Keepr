918f0ce6575c4b9f189f633014af48d54a857a88
Josh Schoenecker <schoeneckerj@gmail.com>
2020-05-26 16:23:52 -0600

    VaultComp restructure&design

--- wwwsrc/src/components/VaultComp.vue
+++ wwwsrc/src/components/VaultComp.vue
@@ -1,35 +1,38 @@
 <template>
   <div class="vault col-sm-10 col-lg-3 m-auto">
-    <div class="card mb-3">
-      <div class="card-header">
-        <!-- TODO drop down elipse for EDIT -->
-        <div class="dropdown show" v-if="$auth.user.sub == vaultData.userId">
+    <div class="card mb-3" style="box-shadow: 1px 1px 10px #369">
+      <!-- TODO drop down elipse for EDIT -->
+      <div class="dropdown show" v-if="$auth.user.sub == vaultData.userId">
+        <button
+          class="bg-transparent border-0"
+          href="#"
+          role="button"
+          id="dropdownMenuLink"
+          data-toggle="dropdown"
+          aria-haspopup="true"
+          aria-expanded="false"
+        >
+          <i class="fas fa-ellipsis-h"></i>
+        </button>
+        <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
           <button
-            class="bg-transparent border-0"
-            href="#"
-            role="button"
-            id="dropdownMenuLink"
-            data-toggle="dropdown"
-            aria-haspopup="true"
-            aria-expanded="false"
-          >
-            <i class="fas fa-ellipsis-h"></i>
-          </button>
-          <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-            <button
-              class="btn text-danger btn-sm ml-4"
-              style="font-size: 1.1rem"
-              @click="deleteVault()"
-              data-toggle="tooltip"
-              data-placement="top"
-              title="Delete This Vault"
-            >Delete Vault</button>
-          </div>
+            class="btn text-danger btn-sm ml-4"
+            style="font-size: 1.1rem"
+            @click="deleteVault()"
+            data-toggle="tooltip"
+            data-placement="top"
+            title="Delete This Vault"
+          >Delete Vault</button>
         </div>
       </div>
-      <div class="card-title">{{vaultData.name}}</div>
-      <div class="card-body">{{vaultData.description}}</div>
-      <div class="card-footer">
+      <div class="card-title text-center mt-2">
+        <h3>{{vaultData.name}}</h3>
+        <hr class="w-25 mt-0 bg-danger" />
+      </div>
+      <div class="card-body">
+        <h5>{{vaultData.description}}</h5>
+      </div>
+      <div class="card-footer text-center mb-1 mx-auto rounded shadow-sm border-0 bg-light">
         <!-- TODO total amount of keeps & create at date -->
       </div>
     </div>
